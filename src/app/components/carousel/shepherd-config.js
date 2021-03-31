c8b092ba2b93c1b8019a5d85903fa9bcc035c5b3
Simon Waiblinger <16225108+simwai@users.noreply.github.com>
2021-03-31 14:40:18 +0200

    optimized shepherd config

--- src/app/components/carousel/shepherd-config.js
+++ src/app/components/carousel/shepherd-config.js
@@ -1,3 +1,35 @@
+const states = {
+  settingsClick: 'settings-click',
+  previousGif: 'previous-gif',
+  nextGif: 'next-gif',
+  settingsAction: 'settings-action'
+}
+
+const setState = (stateName) => {
+  return function() {
+    localStorage.setItem(stateName, 'visited')
+  }
+}
+
+const buttons = (stateName) => [
+  {
+    classes: 'shepherd-button-secondary',
+    text: 'Exit',
+    type: 'cancel'
+  },
+  {
+    classes: 'shepherd-button-primary',
+    text: 'Back',
+    type: 'back'
+  },
+  {
+    classes: 'shepherd-button-primary',
+    text: 'Next',
+    type: 'next',
+    action: setState(stateName)
+  }
+]
+
 export const shepherdConfig = {
   defaultStepOptions: {
     // classes: 'hover-settings previous-gif next-gif',
@@ -7,188 +39,90 @@ export const shepherdConfig = {
     }
   },
   defaultSteps: [{
-    id: 'settings-click',
+    id: states.settingsClick,
     attachTo: {
-      element: '#settings-click',
+      element: '#' + states.settingsClick,
       on: 'bottom'
     },
-    // beforeShowPromise() {
-    //   return new Promise(function(resolve) {
-    //     setTimeout(function() {
-    //       window.scrollTo(0, 0)
-    //       resolve()
-    //     }, 500)
-    //   })
-    // },
-    buttons: [{
-        classes: 'shepherd-button-secondary',
-        text: 'Exit',
-        type: 'cancel'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Back',
-        type: 'back'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Next',
-        type: 'next'
-      }
-    ],
-    cancelIcon: {
-      enabled: true
-    },
-    // classes: 'custom-class-name-1 custom-class-name-2',
-    highlightClass: 'highlight',
-    scrollTo: false,
+    buttons: buttons(states.settingsClick),
     title: 'Welcome to Angular-Shepherd!',
     text: ['Angular-Shepherd is a JavaScript library for guiding users through your Angular app.'],
     when: {
-      show: () => {
-        console.log('show step')
-      },
-      hide: () => {
-        console.log('hide step')
-      }
+      show: setState(states.settingsClick),
+      hide: setState(states.settingsAction)
+    },
+    beforeShowPromise() {
+      return new Promise(function(resolve) {
+        console.log(localStorage.getItem(states.settingsClick))
+        if (localStorage.getItem(states.settingsClick) !== 'visited') {
+          console.log('resolve')
+          resolve()
+        } else {
+          // TODO find out if the thrown error can be prevented by some hacky code
+          reject()
+        }
+      })
+    },
+  }, {
+    id: states.previousGif,
+    attachTo: {
+      element: '#' + states.previousGif,
+      on: 'bottom'
+    },
+    buttons: buttons(states.previousGif),
+    title: 'Welcome to Angular-Shepherd!',
+    text: ['Angular-Shepherd is a JavaScript library for guiding users through your Angular app.'],
+    when: {
+      show: setState(states.previousGif),
+      hide: setState(states.previousGif)
     }
   }, {
-    id: 'previous-gif',
+    id: states.nextGif,
     attachTo: {
-      element: '#previous-gif',
+      element: '#' + states.nextGif,
       on: 'bottom'
     },
-    // beforeShowPromise() {
-    //   return new Promise(function(resolve) {
-    //     setTimeout(function() {
-    //       window.scrollTo(0, 0)
-    //       resolve()
-    //     }, 500)
-    //   })
-    // },
-    buttons: [{
-        classes: 'shepherd-button-secondary',
-        text: 'Exit',
-        type: 'cancel'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Back',
-        type: 'back'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Next',
-        type: 'next'
-      }
-    ],
-    cancelIcon: {
-      enabled: true
-    },
-    // classes: 'custom-class-name-1 custom-class-name-2',
-    highlightClass: 'highlight',
-    scrollTo: false,
+    buttons: buttons(states.nextGif),
     title: 'Welcome to Angular-Shepherd!',
     text: ['Angular-Shepherd is a JavaScript library for guiding users through your Angular app.'],
     when: {
-      show: () => {
-        console.log('show step')
-      },
-      hide: () => {
-        console.log('hide step')
-      }
+      show: setState(states.nextGif),
+      hide: setState(states.nextGif)
     }
   }, {
-    id: 'next-gif',
+    id: states.settingsAction,
     attachTo: {
-      element: '#next-gif',
+      element: '#' + states.settingsAction,
       on: 'bottom'
     },
-    // beforeShowPromise() {
-    //   return new Promise(function(resolve) {
-    //     setTimeout(function() {
-    //       window.scrollTo(0, 0)
-    //       resolve()
-    //     }, 500)
-    //   })
-    // },
-    buttons: [{
-        classes: 'shepherd-button-secondary',
-        text: 'Exit',
-        type: 'cancel'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Back',
-        type: 'back'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Next',
-        type: 'next'
-      }
-    ],
-    cancelIcon: {
-      enabled: true
-    },
-    // classes: 'custom-class-name-1 custom-class-name-2',
-    highlightClass: 'highlight',
-    scrollTo: false,
+    buttons: buttons(states.settingsAction),
     title: 'Welcome to Angular-Shepherd!',
     text: ['Angular-Shepherd is a JavaScript library for guiding users through your Angular app.'],
     when: {
-      show: () => {
-        console.log('show step')
-      },
-      hide: () => {
-        console.log('hide step')
-      }
-    }
-  }, {
-    id: '#settings-action',
-    attachTo: {
-      element: '#settings-action',
-      on: 'bottom'
+      show: setState(states.settingsAction),
+      hide: setState(states.settingsAction)
     },
-    // beforeShowPromise() {
-    //   return new Promise(function(resolve) {
-    //     setTimeout(function() {
-    //       window.scrollTo(0, 0)
-    //       resolve()
-    //     }, 500)
-    //   })
-    // },
-    buttons: [{
-        classes: 'shepherd-button-secondary',
-        text: 'Exit',
-        type: 'cancel'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Back',
-        type: 'back'
-      },
-      {
-        classes: 'shepherd-button-primary',
-        text: 'Next',
-        type: 'next'
-      }
-    ],
-    cancelIcon: {
-      enabled: true
-    },
-    // classes: 'custom-class-name-1 custom-class-name-2',
-    highlightClass: 'highlight',
-    scrollTo: false,
-    title: 'Welcome to Angular-Shepherd!',
-    text: ['Angular-Shepherd is a JavaScript library for guiding users through your Angular app.'],
-    when: {
-      show: () => {
-        console.log('show step')
-      },
-      hide: () => {
-        console.log('hide step')
-      }
-    }
   }]
 }
+
+
+// possible config options
+// beforeShowPromise() {
+//   return new Promise(function(resolve) {
+//     setTimeout(function() {
+//       window.scrollTo(0, 0)
+//       resolve()
+//     }, 500)
+//   })
+// },
+// when: {
+//   show: () => {
+//     console.log('show step')
+//   },
+//   hide: () => {
+//     console.log('hide step')
+//     localStorage.setItem('settings-action', 'visited')
+//   }
+// }
+// classes: 'custom-class-name-1 custom-class-name-2',
+// highlightClass: 'highlight',
