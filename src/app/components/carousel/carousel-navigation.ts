b0e3d8ae65b519547b1b4a82ba06032b2b1435c4
simwai <SimonWaiblinger@live.at>
2021-05-09 09:35:06 +0200

    commit

--- src/app/components/carousel/carousel-navigation.ts
+++ src/app/components/carousel/carousel-navigation.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core'
-import { LocalStorage } from 'ngx-webstorage'
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core'
+import { LocalStorage, LocalStorageService } from 'ngx-webstorage'
+import { GifService } from 'src/app/services/gif.service'
 
-import { SlideshowService } from 'src/app/services/slideshow.service'
 import { environment } from 'src/environments/environment'
 
 @Component({
@@ -10,11 +10,99 @@ import { environment } from 'src/environments/environment'
   styleUrls: ['./carousel-navigation.scss'],
   host: {
     class: 'h-full flex flex-col justify-center items-center'
-  }
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CarouselNavigationComponent {
+export class CarouselNavigationComponent implements OnInit {
   @LocalStorage('bgColor') public bgColor: string
 
-  // tslint:disable-next-line: unnecessary-constructor
-  constructor(public slideshowService: SlideshowService) {}
+  public gifs: string[]
+  public offset: number
+
+  // public isKeywordNew = false
+  private _gifAmount
+  private _index: number
+  private _intervalId: any
+
+  constructor(
+    private readonly _localStorageService: LocalStorageService,
+    private readonly _gifService: GifService,
+    private readonly _ngZone: NgZone,
+    private readonly _cdRef: ChangeDetectorRef
+  ) {}
+
+  public get currentGif(): string {
+    return this.gifs[this.index]
+  }
+
+  public get index(): number {
+    return this._index
+  }
+
+  public set index(value: number) {
+    this._index = value
+    this._cdRef.detectChanges()
+  }
+
+  public loadGifs(): void {
+    let keyword = this._localStorageService.retrieve('keyword')
+
+    if (!keyword) {
+      keyword = environment.keyword
+    }
+
+    console.log(keyword)
+    this._gifService
+      .getGifs(keyword, this._gifAmount, this.offset)
+      .subscribe(data => {
+        this.gifs = data
+        this.restartInterval()
+      })
+  }
+
+  public nextGif(): void {
+    this.restartInterval()
+    this.index++
+
+    // if (this.index + 1 > this.gifs.length - 1) {
+    //   this.loadGifs()
+    // }
+
+    console.log(this.index)
+    // TODO check if gif is available
+  }
+  public ngOnInit(): void {
+    this.gifs = []
+    this.index = 0
+    this.offset = 0
+    this._gifAmount = 50
+
+    this.loadGifs()
+  }
+
+  public previousGif(): void {
+    this.restartInterval()
+    this.index--
+
+    // if (this.index - 1 < 0) {
+    //   console.log(this.index)
+
+    //   return
+    // }
+
+    console.log(this.index)
+  }
+
+  public restartInterval(): void {
+    clearInterval(this._intervalId)
+
+    // prevent change detection bottleneck
+    // https://lukeliutingchun.medium.com/angular-performance-issue-caused-by-settimeout-and-setinterval-1a4a65c07be3
+    this._ngZone.runOutsideAngular(() => {
+      this._intervalId = setInterval(() => {
+        this.index++
+        console.log(this.index)
+      }, this._localStorageService.retrieve('interval') ?? environment.interval * 1000)
+    })
+  }
 }
