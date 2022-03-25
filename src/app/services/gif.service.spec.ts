f2f7a9ad085e271c64023d2065da381013451aca
simwai <SimonWaiblinger@live.at>
2022-03-25 22:00:01 +0100

    fixed test

--- src/app/services/gif.service.spec.ts
+++ src/app/services/gif.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing/'
 import { TestBed } from '@angular/core/testing'
 
 import { GifService } from './gif.service'
@@ -6,7 +7,9 @@ describe('GifService', () => {
   let service: GifService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ })
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+     })
     service = TestBed.inject(GifService)
   })
 
